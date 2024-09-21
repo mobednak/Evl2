ad2c8172e042f4e4dd769aca9fdde826f6e6e9bc
Обедняк Мария Григорьевна <1547-22@std.pkgh.edu>
2024-09-21 11:18:06 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,69 @@
+import kotlin.random.Random
+
+
+fun main() {
+
+        val options = listOf("Камень", "Ножницы", "Бумага")
+
+
+
+        while (true) {
+
+            println("Введите ваш выбор (Камень, Ножницы, Бумага) или 'Выход' для завершения игры:")
+
+            val playerChoice = readLine()
+
+
+
+            if (playerChoice == "Выход") {
+
+                println("Спасибо за игру!")
+
+                break
+
+            }
+
+
+
+            if (playerChoice !in options) {
+
+                println("Некорректный ввод. Попробуйте снова.")
+
+                continue
+
+            }
+
+
+
+            val computerChoice = options[Random.nextInt(options.size)]
+
+
+
+            println("Выбор компьютера: $computerChoice")
+
+            println("Ваш выбор: $playerChoice")
+
+
+
+            when {
+
+                playerChoice == computerChoice -> println("Ничья!")
+
+                (playerChoice == "Камень" && computerChoice == "Ножницы") ||
+
+                        (playerChoice == "Ножницы" && computerChoice == "Бумага") ||
+
+                        (playerChoice == "Бумага" && computerChoice == "Камень") -> println("Вы победили!")
+
+                else -> println("Вы проиграли!")
+
+            }
+
+            println()
+
+        }
+
+    }
+
+
+
